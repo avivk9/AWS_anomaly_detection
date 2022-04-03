48abecf2c8dad03b885d4f45afb46484cc11b599
SheerShveka <sheers2004@gmailcom>
2022-04-03 16:39:21 +0300

    DETAILS

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -1,10 +1,20 @@
+import details
+
+
 class anomalyDetection:
     th = 0.9
     normalModel = []
     def __init__(self):
         self.normalModel = None
     def learnNormal(self,var1,var2):
-        pass
+        for i in range(len(var1)):
+            maxAbsPear =0
+            maxPlace = -1
+            valX = var1
+            for j in range(i+1,len(var2)):
+                valY = var2[j]
+                pear = details.pearson(valX,valY)
+                if(abs(pear) > self.th & abs(pear)>maxAbsPear)
 
 
     def detect(self, times, utilizations):
