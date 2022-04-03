a2fd7a0166cd926b73167cccd7f674180bc5be35
Omer Sherer <omer.sherer141@gmail.com>
2022-04-04 02:03:57 +0300

    commiting...

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import time
 
-from boto3_instance_utilization import getTimes
-from boto3_instance_utilization import getUtilizations
+from boto3_instance_utilization import update_time
+from boto3_instance_utilization import update_cpu
 
 import csv
 
@@ -9,11 +9,9 @@ with open('trycsv.csv', 'w', newline='') as f:
     thewriter = csv.writer(f)
     thewriter.writerow(['times', 'utilizations'])
 
-while(True):
-    with open('trycsv.csv', 'a', newline='') as f:
-        thewriter = csv.writer(f)
-        times = getTimes()
-        utilizations = getUtilizations()
-        for i in range(len(times)):
-            thewriter.writerow([times[i], utilizations[i]])
-    time.sleep(3600)
+with open('trycsv.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    times = update_time()
+    utilizations = update_cpu()
+    for i in range(len(times)):
+        writer.writerow([times[i], utilizations[i]])
