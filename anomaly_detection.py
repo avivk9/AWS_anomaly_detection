1a5465d627923954a9180730542d085e27d9d233
Omer Sherer <omer.sherer141@gmail.com>
2022-04-04 03:25:56 +0300

    final learn normal & detect

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -8,18 +8,19 @@ from typing import List
 
 
 class normal:
-    def __init__(self, top, bot, max_slope, min_slope):
+    def __init__(self, top, bot, max_slope, min_slope, sequence0):
         self.top = top * 1.1
         self.bot = bot * 1.1
         self.max_slope = max_slope * 1.1
         self.min_slope = min_slope * 0.9
+        self.sequence0 = int(sequence0 * 1.1)
 
 
 class anomalyDetection:
     normalModel = []
 
     def __init__(self):
-        self.normalModel = None
+        self.normalModel = normal(0, 0, 0, 0, 0)
 
     @abstractmethod
     def attach(self, observer: controller) -> None:
@@ -46,6 +47,17 @@ class anomalyDetection:
             elif utilizations[i] < bot:
                 bot = utilizations[i]
 
+        # longest sequence of 0 usage
+        count_sequence0 = 0
+        max_sequence0 = 0
+        for i in range(len(utilizations)):
+            if utilizations[i] < 1:  # basically, it's 0 usage
+                count_sequence0 += 1
+            elif count_sequence0 > max_sequence0:
+                max_sequence0 = count_sequence0
+        if count_sequence0 > max_sequence0:
+            max_sequence0 = count_sequence0
+
         for i in range(len(times) - 1):
             slopes[i] = (utilizations[i + 1] - utilizations[i]) / (times[i + 1] - times[i])
 
@@ -58,23 +70,41 @@ class anomalyDetection:
             if slopes[i] < min_slope:
                 min_slope = slopes[i]
 
-        self.normalModel = normal(top, bot, max_slope, min_slope)
+        self.normalModel = normal(top, bot, max_slope, min_slope, max_sequence0)
 
     def detect(self, times, utilizations):
         anomaly = ""
-        last = len(utilizations) - 1
+        last = utilizations[len(utilizations) - 1]
         # 0 usage
-        if utilizations[last] <= 1 and utilizations[last - 1] <= 1 and utilizations[last - 2] <= 1:
+        if self.is_long_sequence0(utilizations):
             return "empty_server"
         # low usage
-        if utilizations[last] <= 30 and utilizations[last - 1] <= 30 and utilizations[last - 2] <= 30:
+        if self.is_long_sequence_low(utilizations):
             return "change_server_host"
+        if last > self.normalModel.max_slope or last < self.normalModel.min_slope:
+            return "exceeded_limit"
+
         # calculate m
-        m = utilizations[last] - utilizations[last - 1] / utilizations[last - 1] - utilizations[last - 2]
+        m = (utilizations[last] - utilizations[last - 1])/(times[last]-times[last-1])
 
         #
-        if m >= 2:
+        if m >= self.normalModel.max_slope:
             return "hacked"
-        if m <= -2:
+        if m <= self.normalModel.min_slope:
             return "server_cooled_hard"
         return "normal_status"
+
+
+    def is_long_sequence0(self, utilizations):
+        last = len(utilizations)
+        flag = True
+        for i in range(self.normalModel.sequence0):
+            flag = flag and utilizations[last - i] <= 1
+        return flag
+
+    def is_long_sequence_low(self, utilizations):
+        last = len(utilizations)
+        flag = True
+        for i in range(self.normalModel.sequence0):
+            flag = flag and utilizations[last - i] <= 30
+        return flag
